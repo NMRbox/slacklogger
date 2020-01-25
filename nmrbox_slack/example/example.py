a049a1ca37abe047dae8665d8082a6beba7fc8c4
Gerard Weatherby <gweatherby@uchc.edu>
2020-01-25 15:05:54 -0500

    Change name to slack-webclient-logger

--- nmrbox_slack/example/example.py
+++ nmrbox_slack/example/example.py
@@ -4,8 +4,8 @@ from nmrbox_slack.slacklogger import SlackHandler
 if __name__ == "__main__":
     logging.basicConfig()
     try:
-        token = 'xoxb-119806757077-466123515045-iouo86q7Eu5Pftczf9z52wX5'
-        channel = 'talktobot'
+        token = 'xoxb-...'
+        channel = 'channel-name'
         handler = SlackHandler(token, channel)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="slacklogger",
+    name="slack-webclient-logger",
     version="1.0.0",
     author="Gerard Weatherby",
     author_email="gweatherby@uchc.edu",
@@ -20,4 +20,5 @@ setuptools.setup(
     ],
     install_requires=['slackclient>=2.50'],
     python_requires='>=3.6',
+    keywords='slack logger'
 )
