36db627a35ccd0bf6deeb48b52978c677eac9399
Gerardwx <gerardw@alum.mit.edu>
2020-04-02 16:07:15 -0400

    Fix setup errors

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
-import setuptools
+from setuptools import setup,find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.rst", "r") as fh:
+    readme_long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="slack-webclient-logger",
-    version="1.0.2",
+    version="1.0.4",
     author="Gerard Weatherby",
     author_email="gweatherby@uchc.edu",
     description="Python logging handler which posts to slack",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=readme_long_description,
     url="https://github.com/NMRbox/slacklogger",
-    packages=setuptools.find_packages(exclude='test.cfg'),
+    packages=find_packages(exclude='test.cfg'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['slackclient>=2.50'],
+    install_requires=['slackclient>=2.5.0'],
     python_requires='>=3.6',
     keywords='slack logger'
 )
