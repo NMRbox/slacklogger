ee69d0cab8457c41ae72373aaaa47a342a28fb9d
Gerardwx <gerardw@alum.mit.edu>
2020-12-04 16:49:57 -0500

    LazySlackClient

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="slack-webclient-logger",
-    version="2.0",
+    version="2.1",
     author="Gerard Weatherby",
     author_email="gweatherby@uchc.edu",
     description="Python logging handler which posts to slack",
